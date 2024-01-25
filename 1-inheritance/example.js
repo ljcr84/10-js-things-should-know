4b2e8ff088c91eb48c363f532b1b0ea64fd32bf0
ljcr84 <66281472+ljcr84@users.noreply.github.com>
2024-01-24 21:39:16 -0300

    Update Inheritance session in the example.js file

--- 1-inheritance/example.js
+++ 1-inheritance/example.js
@@ -1,21 +1,34 @@
 // Inheritance using Classes
+// Note the lack of the keywork 'function'. There is not constructor function
 class Person {
   talk() {
     return 'Talking';
   }
 }
-
+// Writing directly at the browser console the work 'Person' returns a textual representation of it's definition
+// Asking typeof(Person) returns 'function'
+// I tried the following and obtained Person.prototype with a method called 'function'
+class Person {
+  function () {
+    return 'Talking'
+  }
+}
 const me = new Person();
 const you = new Person();
 me.talk(); // Talking
 you.talk(); // Talking
 
 // To update the function for both instances you only have to do it once:
+// The consequence of doing it in this way is that the property Person.prototype.talk.name -> '' and not 'talk' as before
 Person.prototype.talk = function () {
   return 'New and improved Talking';
 }; 
-
-
+// Its better to do this:
+Person.prototype.talk = function talk() {
+  return 'New and improved Talking';
+}
+// But if we type Person in the console, the old definition is still printed
+// But typing Person.prototype.talk give the new definition and is what is passed when using the 'new' keyword
 // -----------------------------------------
 // Inheritance using a Constructor Function
 function Person() {};
